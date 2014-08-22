ac627f76d48aa7a7c790eaf213554f93917d70ab
henriquevieira <henrique.vieira@usp.br>
2014-08-22 00:48:32 -0300

    update all 08-22-2014

--- banco de dados/script_insert_type.sql
+++ banco de dados/script_insert_type.sql
@@ -9,4 +9,11 @@ INSERT INTO tipo_familiar (tipo, descricao) VALUES ("IRMÃO", "IRMÃO");
 INSERT INTO tipo_familiar (tipo, descricao) VALUES ("TIA", "TIA");
 INSERT INTO tipo_familiar (tipo, descricao) VALUES ("TIO", "TIO");
 
+INSERT INTO tipo_identificacao (tipo, descricao) VALUES ("NN", "Numero de cadastro usado no Laboratorio de Patologia Clinica");
+INSERT INTO tipo_identificacao (tipo, descricao) VALUES ("Codigo da familia", "Codificação utilizada para cadastro de familia");
 
+INSERT INTO tipo_amostra (tipo, descricao) VALUES ("SORO", "SORO");
+INSERT INTO tipo_amostra (tipo, descricao) VALUES ("SALIVA", "SALIVA");
+INSERT INTO tipo_amostra (tipo, descricao) VALUES ("TECIDO", "TECIDO");
+
+INSERT INTO tipo_armazenamento (tipo, descricao) VALUES ("PASTA", "Pasta de plastico");
